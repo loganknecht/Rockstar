437aca91c2e2615d3f4edc683c975ce5e772dcfd
Hugbot <hugbot@HugbotMacBook-Pro.local>
2025-01-26 01:19:22 -0800

    cleanup

---
+++ test.py
@@ -0,0 +1,17 @@
+from rockstar import RockStar
+
+python_code = "print('Go to https://www.loganknecht.com, or ask me about my experience directly.')"
+rock_python_bro = RockStar(days=3650, file_name='contact_me.py', code=python_code)
+rock_python_bro.make_me_a_rockstar()
+
+typescript_code = "console.log('Go to https://www.loganknecht.com, or ask me about my experience directly.')"
+rock_typescript_bro = RockStar(days=3650, file_name='contact_me.ts', code=typescript_code)
+rock_typescript_bro.make_me_a_rockstar()
+
+c_sharp_code = "console.log('Go to https://www.loganknecht.com, or ask me about my experience directly.')"
+rock_c_sharp_bro = RockStar(days=3650, file_name='contact_me.cs', code=c_sharp_code)
+rock_c_sharp_bro.make_me_a_rockstar()
+
+cpp_code = "printf('Go to https://www.loganknecht.com, or ask me about my experience directly.')"
+rock_cpp_bro = RockStar(days=3650, file_name='contact_me.cpp', code=cpp_code)
+rock_cpp_bro.make_me_a_rockstar()
